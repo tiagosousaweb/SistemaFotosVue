5c779616aefe99291d2eddf3bd35b729ca71d497
tiagosousaweb <tiagosousaweb@gmail.com>
2021-11-19 12:58:28 -0300

    Primeiro Commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="corpo">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.corpo {
+  font-family: Helvetica, sans-serif;
+  width: 96%;
+  margin: 0 auto;
+}
+</style>
